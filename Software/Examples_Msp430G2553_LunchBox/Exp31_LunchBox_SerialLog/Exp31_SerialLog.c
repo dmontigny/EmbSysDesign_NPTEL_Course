bad6b1236c21801d405812baf57b47666a5dc7e4
dmontigny <dmontigny27@gmail.com>
2020-12-01 18:25:20 -0500

    altered SerialPrint.c to get it working

---
+++ Software/Examples_Msp430G2553_LunchBox/Exp31_LunchBox_SerialLog/Exp31_SerialLog.c
@@ -0,0 +1,59 @@
+#include <msp430.h>
+#include <math.h>
+#include <Library/LunchboxCommon.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+
+#define AIN     BIT0                    // Analog Input at P1.0
+
+volatile float logValues = 0;
+volatile unsigned char number = 1;
+
+/**
+ * @brief
+ * These settings are w.r.t. enabling ADC10 on LunchBox
+ **/
+void register_settings_for_ADC10()
+{
+    ADC10AE0 |= AIN;                            // P1.0 ADC option select
+    ADC10CTL1 = INCH_0;                         // ADC Channel -> 1 (P1.0)
+    ADC10CTL0 = SREF_0 + ADC10SHT_3 + ADC10ON;  // Ref -> Vcc, 64 CLK S&H
+}
+
+
+/*@brief entry point for the code*/
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD;                   // Stop watchdog timer
+
+
+    BCSCTL1 |= (BIT0 + BIT1 + BIT2 + BIT3);     // Selecting RSELx as 15
+    DCOCTL  |= (BIT6 + BIT5 + BIT4);            // Selecting DCOx as 7,
+                                                // DCO_freq = 15.6 MHz
+
+    register_settings_for_ADC10();              // Register setting for ADC10
+
+    // Function to initialize Serial on LunchBox using LunchboxCommon.h
+    initialise_SerialPrint_on_lunchbox();
+
+    unsigned int i;
+    while(1)
+    {
+        ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
+        while(ADC10CTL1 & ADC10BUSY);           // Wait for conversion to end
+
+        int adcValue = ADC10MEM;
+
+        if(adcValue != 0)
+        {
+            logValues = log10(adcValue);      // Taking Log base 10 of ADC value
+            logValues = 2.0 * logValues;
+        }
+
+        number  = 1 * logValues;
+        printf("ADC value : %d, \t(Log base 10) * 2 : %c \r\n", adcValue,
+               number + '0');
+
+        for (i = 0; i < 20000; i++);
+    }
+}
--- Software/Examples_Msp430G2553_LunchBox/Exp31_LunchBox_SerialLog/Library/SerialPrint.c
+++ Software/Examples_Msp430G2553_LunchBox/Exp31_LunchBox_SerialLog/Library/SerialPrint.c
@@ -53,6 +53,24 @@ int fputs(const char *_ptr, register FILE *_fp)
  **/
 void initialise_SerialPrint_on_lunchbox()
 {
+    WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+    if (CALBC1_1MHZ==0xFF)                    // If calibration constant erased
+    {
+        while(1);                               // do not load, trap CPU!!
+    }
+    DCOCTL = 0;                               // Select lowest DCOx and MODx settings
+    BCSCTL1 = CALBC1_1MHZ;                    // Set DCO
+    DCOCTL = CALDCO_1MHZ;
+    P1SEL = BIT1 + BIT2 ;                     // P1.1 = RXD, P1.2=TXD
+    P1SEL2 = BIT1 + BIT2 ;                    // P1.1 = RXD, P1.2=TXD
+    UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+    UCA0BR0 = 104;                            // 1MHz 9600
+    UCA0BR1 = 0;                              // 1MHz 9600
+    UCA0MCTL = UCBRS0;                        // Modulation UCBRSx = 1
+    UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+    IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
+
+/*
     P1SEL = BIT1 + BIT2;              // Select UART RX/TX function on P1.1,P1.2
     P1SEL2 = BIT1 + BIT2;
 
@@ -63,4 +81,6 @@ void initialise_SerialPrint_on_lunchbox()
     UCA0MCTL &= ~UCOS16;
     UCA0CTL1 &= ~UCSWRST;               // Initialize UART Module
     IE2 |= UCA0RXIE;                    // Enable RX interrupt
+*/
 }
+
